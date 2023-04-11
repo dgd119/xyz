e3827a40853a43b1c2f6d09c948fda7f3053b96d
dgd119 <dgdevans@gmail.com>
2023-04-11 12:26:53 +0100

    Update eleventy.config.socialshare.js

--- eleventy.config.socialshare.js
+++ eleventy.config.socialshare.js
@@ -4,7 +4,7 @@ async function shareImageShortcode(src) {
   // src might be small.png - taken from frontmatter
   const { url } = this.page;
   // url might be /blog/hello-world/
-  const imageSrc = "./blog/" + src;
+  const imageSrc = "./content/blog/" + src;
 	// const imageSrc = "./blog/fourthpost/IMG_6055.png"
   let metadata = await Image(imageSrc, {
     widths: [600],
