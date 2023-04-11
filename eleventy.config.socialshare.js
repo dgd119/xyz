944ae5d1a14b3257daf648f20959170e92ac7f14
dgd119 <dgdevans@gmail.com>
2023-04-11 12:42:22 +0100

    Update eleventy.config.socialshare.js

--- eleventy.config.socialshare.js
+++ eleventy.config.socialshare.js
@@ -9,8 +9,8 @@ async function shareImageShortcode(src) {
   let metadata = await Image(imageSrc, {
     widths: [600],
     formats: ["jpeg"],
-    urlPath: "/img/",
-    // outputDir: "./img/",
+    //urlPath: "/img/",
+    outputDir: "./content/blog/",
   });
 
   const data = metadata.jpeg[0];
